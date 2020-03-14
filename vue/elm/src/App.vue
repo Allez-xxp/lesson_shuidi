62a5704837ecefb1a83e4be41904707fb3ce064b
Allez-xxp <2811632058@qq.com>
2020-03-14 22:27:40 +0800

    alert组件，emit用法

--- vue/elm/src/App.vue
+++ vue/elm/src/App.vue
@@ -7,20 +7,49 @@
     </div>
     <router-view /> -->
     <!-- 首页head -->
-    <head-top headTitle="东华理工大学">
+    <!-- <head-top headTitle="东华理工大学">
       <span class="head_logo" slot="logo">ele.me</span>
-    </head-top>
+    </head-top> -->
     <!-- 地址head -->
-    <head-top head-title="编辑地址" go-back="true">
+    <!-- <head-top head-title="编辑地址" go-back="true">
       <span class="edit" slot="edit">编辑</span>
-    </head-top>
+    </head-top> -->
+    <!-- alert 通用功能，引入 -->
+    <!-- 1.alert 需要时才出现，点击按钮后触发 -->
+    <!-- 提示消息；@：自定义事件 -->
+    <alert-tip v-if="showAlert"
+     :alertText="alertText" @closeTip="closeAlert"/>
+    <button @click="showCancelAlert">提示退出</button>
+    <button @click="showLoginAlert">提示登录</button>
   </div>
 </template>
 <script>
-import Header from './components/header/header';
+// import Header from './components/header/header';
+import AlertTip from './components/common/alertTip';
 export default {
+  data() {
+    return {
+      showAlert: false,
+      alertText: '',
+       
+    }
+  },
+  methods: {
+    closeAlert() {
+      this.showAlert = false
+    },
+    showCancelAlert() {
+      this.alertText = '您真的要退出吗？'
+      this.showAlert = true 
+    },
+    showLoginAlert() {
+      this.alertText = '您还没有登录吗？'
+      this.showAlert = true 
+    },
+  },
   components: {
-    "head-top": Header
+    // "head-top": Header
+    'alert-tip': AlertTip
   }
 }
 </script>
---
+++ vue/elm/src/components/common/alertTip.vue
@@ -0,0 +1,85 @@
+<template>
+    <div class="alert_container">
+        <section class="tip_text_container">
+            <div class="tip_icon">
+                <span></span>
+                <span></span>
+            </div>
+            <p class="tip_text">{{alertText}} </p>
+            <div class="confirm" @click="closeTip">确认</div>
+        </section>
+        
+        <!-- 弹窗的确定功能，取消功能；点完关闭它
+        如何关闭？ 通知父组件将showAlert设置为false
+         -->
+        
+    </div>
+</template>
+<script>
+// 数据驱动界面，数据只有data吗？
+// props是一个数据由父组件传给子组件；使通用组件更有意义
+// 
+export default {
+    props: [  //定义外界给它什么
+        'alertText'
+    ], 
+    data() {   //私有数据；页面级组件持有数据，派发给子组件
+        return {
+
+        }
+    },
+    methods: {
+        closeTip() {
+            //emit：通知父组件功能
+            this.$emit('closeTip') 
+        }
+    },
+    // computed() { //计算属性
+
+    // }
+}
+</script>
+<style lang="stylus" scoped>
+@import '../../style/mixin';
+.alert_container
+  position fixed
+  top 0
+  left 0
+  right 0
+  bottom 0
+  z-index 200
+  background-color rgba(0,0,0,.3)
+  .tip_text_container
+    position absolute
+    top 50%
+    left 50%
+    margin-top -6rem
+    margin-left -6rem
+    width 12rem
+    animation tipMove .4s
+    background-color rgba(255, 255, 255, 1)
+    border 1px
+    padding-top 0.6rem
+    display flex
+    justify-content center
+    align-items center
+    flex-direction column
+    border-radius 0.25rem
+    .tip_icon
+      wh(3rem, 3rem)
+      border 0.15rem solid #f8cb86
+      border-radius 50%
+      display flex
+      justify-content center
+      align-items center
+      flex-direction column
+      span:nth-of-type(1)
+        wh(.12rem, 1.5rem)
+        background-color #f8cb86
+      span:nth-of-type(2)
+        wh(.2rem, .2rem)
+        border 1px
+        border-radius 50%
+        margin-top 0.2rem
+        background-color #f8cb8b
+</style>
