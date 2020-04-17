c583968dc327c0cd397471271fa8901f8bd41390
Allez-xxp <2811632058@qq.com>
2020-04-17 20:30:05 +0800

    力扣题库学习3 删除排序链表中的重复元素

---
+++ leetcode/practise/3.js
@@ -0,0 +1,13 @@
+// 删除排序链表中的重复元素
+var deleteDuplicates = function(head) {
+    var cur = head;
+    while(cur && cur.next) {
+        if(cur.val == cur.next.val) {
+            cur.next = cur.next.next;
+        } else {
+            cur = cur.next;
+        }
+    }
+    return head;
+};
+
