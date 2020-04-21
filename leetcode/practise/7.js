cf59d105c398ca035fc47aa7d580dd28b82bf619
Allez-xxp <2811632058@qq.com>
2020-04-21 20:21:31 +0800

    力扣题库学习7 二叉树的所有节点

---
+++ leetcode/practise/7.js
@@ -0,0 +1,9 @@
+// 二叉树的所有路径
+var binaryTreePaths = function(root) {
+    if (!root) return []
+    if (root.left === null && root.right === null) return [root.val.toString()]
+    let left = binaryTreePaths(root.left)
+    let right = binaryTreePaths(root.right)
+    return left.concat(right).map(item => root.val + '->' + item)
+}
+
