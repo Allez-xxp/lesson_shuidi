6c51b1cbd0b66eb731f95760e8eba89addc60f4d
Allez-xxp <2811632058@qq.com>
2020-05-16 16:46:32 +0800

    小程序全栈项目5 首页设计之轮播图设置

--- xcx/mpvue-shop/src/pages/index/index.vue
+++ xcx/mpvue-shop/src/pages/index/index.vue
@@ -9,16 +9,39 @@
         <span class="icon"></span>
       </div>
     </div>
+    <div class="swiper">
+      <!--swiper用于轮播图；indicator-dots：用于是否展示轮播图上的点 
+          autoplay: 是否自动轮播；interval：轮播时间；
+          circular：循环不间断轮播；duration: 轮播图之间的切换时间
+      -->
+      <swiper class="swiper-container" 
+      indicator-dots="true"
+      autoplay="ture"
+      interval="3000"
+      circular="true"
+      duration="500">
+        <block v-for="(item,index) in banner" :key="index">
+          <!-- 用于for循环 
+          swiper-item: 每一张轮播图
+          -->
+          <swiper-item class="swiper-item">
+            <image class="slide-image" :src="item.image_url"/>
+          </swiper-item>
+        </block>
+      </swiper>
+    </div>
   </div>
 </template>
 
 <script>
 import amapFile from '../../utils/amap-wx.js'
 import {mapState, mapMutations} from 'vuex'
+import { get } from '../../utils/index'
 export default {
   data() {  //数据源
     return {
       // cityName: "南昌",
+      banner:[]
     }
   },
   computed: {
@@ -71,6 +94,23 @@ export default {
           _this.update({cityName:'北京'})
         }
       })
+    },
+    // 获取banner数据方法; 异步
+    async getData() {
+      // 调用封装接口请求
+      // wx.request 封装接口请求，去utils/index.js: 封装接口请求
+      // 这里是取数据源的，那就应该调用刚写的接口，那么就需要在当前页面引入get,post这两个方法
+      // 导航的banner图我们采用get方法，那么导入get
+      // 此时的get的url可以写成任何样子，因为此处的路径是等会做后端开发时自己定义的路径，记得等会要与后端的一致
+      const data=await get('/index/index') //后端必须有一个http://localhost:5757/lm/index/index
+      // 这里的get方法是封装的请求
+      // 拿数据
+      console.log(data)//现在还看不到拿到的数据是什么，因为还没写后端的项目
+      // 那么开始后端 用koa2提供/index/index接口给我们这里使用
+      // 数据库的数据能输出了之后，接下来打通banner图的前后端协作连接，将数据成功渲染到页面中去。
+      //把拿到的数据给到数据源中的banner[]
+      this.banner = data.banner
+
     }
   }
 }
--- xcx/mpvue-shop/src/utils/index.js
+++ xcx/mpvue-shop/src/utils/index.js
@@ -1,4 +1,4 @@
-function formatNumber (n) {
+function formatNumber (n) {  //格式化时间
   const str = n.toString()
   return str[1] ? str : `0${str}`
 }
@@ -16,6 +16,48 @@ export function formatTime (date) {
   const t2 = [hour, minute, second].map(formatNumber).join(':')
 
   return `${t1} ${t2}`
+}
+
+// ------------请求封装
+const host = "http:/localhost:5757/lm"
+export { host };
+//请求封装，封装接口请求一般都是封装成一个方法
+// header={}请求头，要不要都行
+function request(url, method, data, header={}) {
+  //每次请求时都会耗时的，为了提高用户体验感，我们加一个loading
+  wx.showLoading({
+    title:"正在玩命加载中...",
+  });
+
+  // 把wx.request接口请求封装成promise data是要向后端接口传的参数
+  // 把wx.request接口请求封装成function request这个方法，以后用直接调用
+  return new Promise((resolve,reject) => {
+    wx.request({
+      url: host + url,
+      method: data,
+      header:{
+        "content-type": "application/json" //默认的请求头
+      },
+      success(res){
+        wx.hideLoading();//把之前的页面加载中隐藏掉
+        resolve(res.data) //这里的data跟data:data的是不一样的，这是response返回值里的data
+      },
+      fail(error){
+        wx.hideLoading();
+        reject(false)
+      },
+      complete (){
+        wx.hideLoading();
+      }
+    })
+  })
+}
+//把get,post方法也封装一下（本栈全部的接口请求只用都用GET，或POST方法）
+export function get(url,data) {
+  return request(url,'GET', data)  //调用request方法
+}
+export function post(url,data) {
+  return request(url,'POST', data)  //调用request方法
 }
 
 export default {
