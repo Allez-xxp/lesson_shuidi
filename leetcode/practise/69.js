33bd342f23925528693c6c9364532306ca3f6b86
Allez-xxp <2811632058@qq.com>
2020-05-19 22:26:26 +0800

    力扣题库 69求平方根

---
+++ leetcode/practise/69.js
@@ -0,0 +1,9 @@
+// 实现 int sqrt(int x) 函数。
+// 计算并返回 x 的平方根，其中 x 是非负整数。
+// 由于返回类型是整数，结果只保留整数的部分，小数部分将被舍去。
+
+var mySqrt = function(x) {
+    // 直接使用Math.sqrt(),计算平方根
+    return parseInt(Math.sqrt(x));
+};
+console.log(mySqrt(8));
