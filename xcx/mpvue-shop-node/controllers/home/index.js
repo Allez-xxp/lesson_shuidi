c57ee2b1477315bbb8cd5a7b2bf5eb5e1dcf4a85
Allez-xxp <2811632058@qq.com>
2020-05-21 16:52:47 +0800

    小程序全栈项目10 首页设计之页面板块设计，专题精选，各种类型好物

--- xcx/mpvue-shop-node/controllers/home/index.js
+++ xcx/mpvue-shop-node/controllers/home/index.js
@@ -26,12 +26,39 @@ module.exports=async (ctx) =>{
     }).limit(5).select()
     //column:分别出字段；limit(5)：只查出5个数据
 
+    // 6. 专题精选
+    const topicList = await mysql('nideshop_topic').limit(3).select()
+
+    // 类别列表（各种好物）
+    const categoryList = await mysql('nideshop_category').where({
+        parent_id: 0
+    }).select()
+    const newCategoryList = []
+
+    for(let i = 0; i < categoryList.length; i++) {
+        let item = categoryList[i]
+        let childCategoryIds = await mysql('nideshop_category').where({
+            parent_id: item.id
+        }).column('id').select()
+        childCategoryIds = childCategoryIds.map((item) => {
+            return item.id
+        })
+        const categoryGoods = await mysql('nideshop_goods').column('id','name','list_pic_url','retail_price').whereIn('category_id',childCategoryIds).limit(7).select()
+        newCategoryList.push({
+            'id':item.id,
+            'name':item.name,
+            'goodsList':categoryGoods
+        })
+    }
+
     ctx.body={
         'banner':banner,
         'channel':channel,
         'brandList':brandList,
         'newGoods':newGoods,
-        'hotGoods':hotGoods
+        'hotGoods':hotGoods,
+        'topicList':topicList,
+        'newCategoryList':newCategoryList
     }
 }
     
--- xcx/mpvue-shop/src/pages/index/index.vue
+++ xcx/mpvue-shop/src/pages/index/index.vue
@@ -96,6 +96,48 @@
         </ul>
       </div>
     </div>
+    <div class="topicList">
+      <div class="topicList-top">
+        专题精选
+        <span class="icon"></span>
+      </div>
+      <div class="list">
+        <ul>
+          <scroll-view class="scroll-view" :scroll-x="true"> 
+            <li v-for="(item, index) in topicList" :key="index" @click="topicdetail(item.id)">
+              <img :src="item.item_pic_url" alt="">
+              <div class="btom">
+                <div>
+                  <p>{{item.title}}</p>
+                  <p>{{item.subtitle}}</p>
+                </div>
+                <div>
+                  {{item.price_info}}元起
+                </div>
+              </div>
+            </li>
+          </scroll-view>
+        </ul>
+      </div>
+    </div>
+    <div class="newcategory">
+      <div class="list" v-for="(item, index) in newCategoryList" :key="index">
+        <div class="head">{{item.name}}好物</div>
+        <div class="sublist">
+          <div v-for="(subitem,subindex) in item.goodsList" :key="subindex">
+            <img :src="subitem.list_pic_url" alt="">
+            <p>{{subitem.name}}</p>
+            <p>{{subitem.retail_price}}</p>
+          </div>
+          <div>
+            <div class="last">
+              <p>{{item.name}}好物</p>
+              <span class="icon"></span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -111,7 +153,9 @@ export default {
       channel:[],
       brandList:[],
       newGoods:[],
-      hotGoods:[]
+      hotGoods:[],
+      topicList:[],
+      newCategoryList:[]
     }
   },
   computed: {
@@ -186,7 +230,9 @@ export default {
       this.channel = data.channel,
       this.brandList = data.brandList,
       this.newGoods = data.newGoods,
-      this.hotGoods = data.hotGoods
+      this.hotGoods = data.hotGoods,
+      this.topicList = data.topicList,
+      this.newCategoryList = data.newCategoryList
     },
     categroyList(id) {
       wx.navigateTo({ //小程序：跳转下一个页面：navigateTo()
@@ -213,6 +259,11 @@ export default {
           url: '/pages/newgoods/main?isNew=' + 1 
         })
       }
+    },
+    topicdetail(id) {
+      wx.navigateTo({ 
+        url: '/pages/topicdetail/main?id' + id 
+      });
     }
   }
 }
