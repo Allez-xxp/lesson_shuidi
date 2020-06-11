f2abee314b2280a8aaba0ee6cc988eee4dd0187b
Allez-xxp <2811632058@qq.com>
2020-06-11 22:35:22 +0800

    力扣27 移除元素

---
+++ leetcode/practise/27/index.js
@@ -0,0 +1,11 @@
+var removeElement = function(nums, val) {
+    let ans = 0;
+    for(const num of nums) {
+        if(num != val) {
+            nums[ans] = num;
+            ans++;
+        }
+    }
+    return ans;
+};
+
